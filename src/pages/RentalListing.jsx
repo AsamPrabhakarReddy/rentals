b69a061aa6e286ca83ce5b4425db9dba399de3ec
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2024-10-28 11:00:15 +0530

    adding components

---
+++ src/pages/RentalListing.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import NavBar from '../components/Navbar'
+import RentalListing1 from '../components/RentalListing1'
+const RentalListing = () => {
+  return (
+    <div>
+        <NavBar/>
+        <RentalListing1/>
+    </div>
+  )
+}
+
+export default RentalListing
